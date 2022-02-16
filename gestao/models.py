f096d9a2cd2000224bfdb8c3951b311ca7c694b1
marilia.neves <marilia.neves@dextra-sw.com>
2022-02-16 16:10:35 -0300

    correção Evento

--- gestao/models.py
+++ gestao/models.py
@@ -5,6 +5,11 @@ Flag = (
     ("NAO", "Não"),
 )
 
+TipoEvento = (
+    ("JOGO", "Jogo"),
+    ("LUTA", "Luta"),
+)
+
 TipoCampeonato = (
     ("ESTADUAL", "ESTADUAL"),
     ("NACIONAL", "NACIONAL"),
@@ -46,17 +51,6 @@ class Cidade(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     estado = models.CharField(max_length=2, choices=Estado)
-
-    def __str__(self):
-        return self.nome
-
-
-class Evento(models.Model):
-    id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=300, null=True)
-    horarioAlterado = models.CharField(max_length=3, choices=Flag)
-    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
@@ -123,6 +117,18 @@ class Jogo(models.Model):
     estado = models.CharField(max_length=3, choices=Estado)
     classificacaoJogo = models.CharField(max_length=100)
     idSde = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Evento(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=300, null=True)
+    horarioAlterado = models.CharField(max_length=3, choices=Flag)
+    modified = models.DateTimeField(auto_now=True)
+    tipoEvento = models.CharField(max_length=15, choices=TipoEvento)
 
     def __str__(self):
         return self.nome
